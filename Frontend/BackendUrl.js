f108b6797070c4507a7dd9ea324ce5cebb37c4a1
meashishpatel <ashishpatel3009@gmail.com>
2025-01-08 20:48:24 +0530

    feat: Update Backend URL to include API version

--- Frontend/BackendUrl.js
+++ Frontend/BackendUrl.js
@@ -1,4 +1,4 @@
 // const Backend_Url = "https://paytm-clone-980b.onrender.com/api/v1";
 // const Backend_Url = "http://localhost:5321/api/v1"
-const Backend_Url = "https://ewallet-0aaf.onrender.com";
+const Backend_Url = "https://ewallet-0aaf.onrender.com/api/v1";
 export default Backend_Url;
