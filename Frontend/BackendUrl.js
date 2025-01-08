df99d6d0ae4228402a7791fb8f865c25cfc2a032
meashishpatel <ashishpatel3009@gmail.com>
2025-01-08 20:44:44 +0530

    feat: Update Backend URL and package-lock.json dependencies

--- Frontend/BackendUrl.js
+++ Frontend/BackendUrl.js
@@ -1,3 +1,4 @@
-const Backend_Url = "https://paytm-clone-980b.onrender.com/api/v1"
-// const Backend_Url = ""
+// const Backend_Url = "https://paytm-clone-980b.onrender.com/api/v1";
+// const Backend_Url = "http://localhost:5321/api/v1"
+const Backend_Url = "https://ewallet-0aaf.onrender.com";
 export default Backend_Url;
