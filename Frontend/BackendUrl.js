3dfcc9f77b5e44aec49c319e39117f9408b2d3c1
meashishpatel <ashishpatel3009@gmail.com>
2025-01-08 20:49:46 +0530

    feat: Simplify Backend URL configuration in Frontend

--- Frontend/BackendUrl.js
+++ Frontend/BackendUrl.js
@@ -1,4 +1,3 @@
-// const Backend_Url = "https://paytm-clone-980b.onrender.com/api/v1";
+const Backend_Url = "https://paytm-clone-980b.onrender.com/api/v1";
 // const Backend_Url = "http://localhost:5321/api/v1"
-const Backend_Url = "https://ewallet-0aaf.onrender.com/api/v1";
 export default Backend_Url;
